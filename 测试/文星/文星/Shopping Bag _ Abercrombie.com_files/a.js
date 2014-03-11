b656a53ca92f481921191c6540bf32635b925091
yuluyi <yuluyi1991@gmail.com>
2014-03-11 14:53:35 +0800

    删除无关文件

--- 测试/文星/文星/Shopping Bag _ Abercrombie.com_files/a.js
+++
@@ -1,1 +0,0 @@
-lpConnLib.Process({"ResultSet": {"lpCallId":"371661563343-608664214964","lpCallConfirm":"","lpJS_Execute":[{"code_id": "INPAGE-DELAY-10", "js_code": "lpMTag.lpInPageRequestDelay=10;"},{"code_id": "FPCSetVal", "js_code": "lpMTagConfig.useFirstParty = true;"}]}});
